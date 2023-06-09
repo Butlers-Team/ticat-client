ede856876f5111db1577b7660d3666a03021b239
parksubeom <sooknise@naver.com>
2023-06-10 08:55:03 +0900

    init : craco 이용하여 tsx환경 절대경로  설정 및 prettier/eslint

---
+++ client/craco.config.js
@@ -0,0 +1,12 @@
+import CracoAlias from "craco-alias";
+
+export const plugins = [
+  {
+    plugin: CracoAlias,
+    options: {
+      source: "tsconfig",
+      baseUrl: "./src",
+      tsConfigPath: "./tsconfig.paths.json",
+    },
+  },
+];
