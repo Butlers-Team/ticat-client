2a35faf6f1dda5df7d350cedfd2ed52da5864b59
sineTlsl <limsieun0313@gmail.com>
2023-06-14 01:24:02 +0900

    design: 모바일 사이즈로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div></div>;
+  return <div className="app"></div>;
 }
 
 export default App;
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -12,48 +12,63 @@ const GlobalStyles = createGlobalStyle`
   --background-color: rgba(255,255,255,1); 
 }
 
-
 ${reset}
 	// 아래에 전역 스타일을 추가
-    *{
-        box-sizing: border-box;
-    }
-    a{
-        text-decoration: none;
-        color: inherit;
-    }
-    html, body, #root {
-        height: 100%;
-    }
-    html,
-    body,
-    body > div { /* the react root */
-        margin: 0;
-        padding: 0;
-        height: 100%;
-    }
-    body {
-        font-family: 'Noto Sans KR', sans-serif;
-    }
-    #root {
-        display: flex;
-    }
-    h2 {
-        margin: 0;
-        font-size: 16px;
-    }
-    ul {
-        margin: 0;
-        padding: 0 0 0 1.5em;
-    }
-    li {
-        padding: 0;
-    }
-    b { 
-        margin-right: 3px;
-    }
+  *{
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;  
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  html {
+    background-color: #f6f6f6;
+    font-size: 62.5%;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  html,
+  body,
+  body > div { /* the react root */
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+  body {
+    padding-top: env(safe-area-inset-top);
+    padding-bottom: env(safe-area-inset-bottom);
+    padding-left: env(safe-area-inset-left);
+    padding-right: env(safe-area-inset-right);
+    max-width: 50rem;
+    margin: 0 auto;
+    height: 100vh;
+    overflow-y: scroll;
+  }
+  #root {
+    display: flex;
+  }
+  h2 {
+    margin: 0;
+    font-size: 16px;
+  }
+  ul {
+    margin: 0;
+    padding: 0 0 0 1.5em;
+  }
+  li {
+    padding: 0;
+  }
+  b { 
+    margin-right: 3px;
+  }
 
-
-	`;
+  .app {
+    height: 100vh;
+    width: 100%;
+    background-color: #fff;
+  }
+`;
 
 export default GlobalStyles;
