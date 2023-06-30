7c7a619ebf8b31184be2fd82f6a3d1e4a772cb0a
sineTlsl <limsieun0313@gmail.com>
2023-06-30 14:23:42 +0900

    style: 버튼 컴포넌트 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import Router from './Router';
-import Button from '@components/Button';
+
 function App() {
   return (
     <div className="app">
-      <Button>버튼</Button>
       <Router />
     </div>
   );
