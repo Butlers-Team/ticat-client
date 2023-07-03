a2d3d1a41e58ce627ae0d5443d3ce0a6113e26dc
sineTlsl <limsieun0313@gmail.com>
2023-07-03 21:49:43 +0900

    feat: navBar가 필요하지 않는 페이지 기능 추가

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,18 @@
+import { useLocation } from 'react-router-dom';
 import TabNav from '@layout/tab-nav';
 import Main from '@layout/main';
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const location = useLocation();
+  const hidePages = ['/stamp/valid', '/stamp/list']; // 숨기고 싶은 페이지
+
+  /** 2023/07/03 - 페이지를 숨기고 싶은 함수 - by sineTlsl */
+  const shouldHide = () => hidePages.includes(location.pathname);
+
   return (
     <>
-      <Main>{children}</Main>
-      <TabNav />
+      <Main className={shouldHide() ? undefined : 'main_vh70'}>{children}</Main>
+      {!shouldHide() && <TabNav />}
     </>
   );
 };
