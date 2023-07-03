8b14309630344d8923e211c66a6f83427b2e2282
sineTlsl <limsieun0313@gmail.com>
2023-07-03 21:08:14 +0900

    fix: 브라우저 주소표시줄 가려짐 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import Router from './Router';
 
 function App() {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    const handleResize = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="app">
       <Router />
