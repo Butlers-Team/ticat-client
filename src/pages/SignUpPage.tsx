aa06160636df422a59c4d193182296d457bc98c9
leekoby <abc2752@naver.com>
2023-07-03 15:20:16 +0900

    feat: 회원가입 페이지 생성

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,8 @@
+import SignUp from '@components/sign-up/SignUp';
+
+/** 2023/06/29 - 회원가입 페이지 - by leekoby */
+const SignUpPage: React.FC = (props): JSX.Element => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
