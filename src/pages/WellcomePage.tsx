28a9e1885b17041ff5009fe863161ae14221c8ea
leekoby <abc2752@naver.com>
2023-07-05 14:56:37 +0900

    feat: wellcome 페이지

---
+++ src/pages/WellcomePage.tsx
@@ -0,0 +1,8 @@
+import Wellcome from '@components/Wellcome';
+
+/** 2023/07/05 - 회원가입 결과 페이지 - by leekoby */
+const WellcomePage: React.FC = (): JSX.Element => {
+  return <Wellcome />;
+};
+
+export default WellcomePage;
