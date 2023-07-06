f33e49833237367dc16c7d8c664de93740c2999b
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-07-06 21:53:09 +0900

    refactor : 축제 디테일 페이지 라우터 경로 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,7 @@ import CalendarPage from '@pages/CalendarPage';
 import StampCheck from '@pages/StampValid';
 import StampList from '@pages/StampList';
 import MainPage from '@pages/MainPage';
+import DetailPage from '@pages/DetailPage';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
       <Routes>
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/detail" element={<DetailPage />} />
         <Route path="/stamp/valid" element={<StampCheck />} />
         <Route path="/stamp/list" element={<StampList />} />
       </Routes>
