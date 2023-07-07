f9db5efcea5643faf3a1395f8f22c483c5b8446e
leekoby <abc2752@naver.com>
2023-07-07 20:34:09 +0900

    feat: ChakraProvider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from '@styles/GlobalStyles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ChakraProvider } from '@chakra-ui/react';
 
 //React Query를 여기다가 설정
 //리액트 쿼리의 핵심 클래스 : 쿼리랑 뮤테이션 같은 것들을 관리하고 캐시해서 받아옴/
@@ -16,7 +17,9 @@ root.render(
     <BrowserRouter>
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
