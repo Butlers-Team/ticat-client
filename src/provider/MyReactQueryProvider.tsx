6262cb01a9109028b7805e7a889a89f25c6677c7
leekoby <abc2752@naver.com>
2023-07-15 00:03:22 +0900

    comment: reactquery defaultOption 주석 처리

--- src/provider/MyReactQueryProvider.tsx
+++ src/provider/MyReactQueryProvider.tsx
@@ -3,12 +3,12 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 /** 2023/07/10 - 전역 설정 적용 - by leekoby */
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 10, // 10 분
-      cacheTime: 1000 * 60 * 15, // 15 분
-    },
-  },
+  // defaultOptions: {
+  //   queries: {
+  //     staleTime: 1000 * 60 * 10, // 10 분
+  //     cacheTime: 1000 * 60 * 15, // 15 분
+  //   },
+  // },
 });
 
 /** 2023/07/10 - "react-query" Provider 적용 - by leekoby */
