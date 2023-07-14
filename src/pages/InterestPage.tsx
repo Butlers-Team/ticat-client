3c0eb828bfc526068676d3d9b1463103de3a3e04
leekoby <abc2752@naver.com>
2023-07-15 03:30:57 +0900

    feat: 관심사페이지 조건부 렌더링

--- src/pages/InterestPage.tsx
+++ src/pages/InterestPage.tsx
@@ -1,10 +1,12 @@
 import Interest from '@components/interest/Interest';
+import { useTokenStore } from '@store/authStore';
 
 interface Props {}
 
 /** 2023/07/14 - 관심사등록 Page - by leekoby */
 const InterestPage: React.FC<Props> = (props): JSX.Element => {
-  return <Interest />;
+  const { accessToken, refreshToken } = useTokenStore();
+  return <>{accessToken && refreshToken && <Interest />}</>;
 };
 
 export default InterestPage;
