76aefebbb894eb623c5f47c149bceffebae512e6
sineTlsl <limsieun0313@gmail.com>
2023-07-21 17:56:34 +0900

    feat: 설정 페이지 추가

---
+++ src/pages/SettingPage.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+// components
+import TopHistoryBackNav from '@components/TopHistoryBackNav';
+
+/** 2023/07/21 - 설정 페이지 - by sineTlsl */
+const SettingPage = () => {
+  const navigate = useNavigate();
+
+  /** 2023/07/21 - 이전 페이지 이동 함수 - by sineTlsl */
+  const goBackPage = () => {
+    navigate('/myinfo');
+  };
+
+  return (
+    <SettingContainer>
+      <TopHistoryBackNav textTitle="설정 관리" onNavigation={goBackPage} />
+    </SettingContainer>
+  );
+};
+
+export default SettingPage;
+
+const SettingContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
