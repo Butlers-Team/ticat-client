a4f6d93923e79655c4a87e393e26fc1d7c5ba916
leekoby <abc2752@naver.com>
2023-07-23 00:20:46 +0900

    fix: index  export 수정

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,6 +1,10 @@
 export * from './category';
 export * from './festival';
 export * from './interest';
+export * from './blogposts';
+export * from './comment';
+export * from './comment-like';
+export * from './comment-dislike';
 
 /** 2023/07/04 - 페이지 데이터 Type - by sineTlsl */
 export interface PageInfoType {
