4a8acbc676ee062f4351200084a7c300b136e013
leekoby <abc2752@naver.com>
2023-07-23 00:16:56 +0900

    feat: 멤버 저장 Store

---
+++ src/store/useMemberStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { Member } from 'types/auth';
+import { persist } from 'zustand/middleware';
+
+export interface MemberInfo {
+  member?: Member | null;
+  setMember: (member: Member | null) => void;
+  resetMember: () => void;
+}
+
+// 초기값 설정
+const initialState: MemberInfo = {
+  member: null,
+  setMember: () => {},
+  resetMember: () => {},
+};
+// resetMember를 외부에서 사용하기 위한 함수
+const resetMember = () => {
+  const { resetMember } = useMemberStore.getState();
+  resetMember();
+};
+/** 2023/07/22 - 로그인 응답 데이터 저장소 - by leekoby */
+const useMemberStore = create(
+  persist<MemberInfo>(
+    set => ({
+      ...initialState,
+      setMember: member => set(state => ({ ...state, member: member ? { ...state.member, ...member } : null })),
+      resetMember: () => set(() => initialState),
+    }),
+    { name: 'member', getStorage: () => localStorage },
+  ),
+);
+
+export { useMemberStore, resetMember };
