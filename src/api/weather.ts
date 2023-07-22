d65f25150c17329fda155ae517aa3eab413b84d8
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-07-22 16:50:42 +0900

    refactor : 축제상세페이지/축제추천/날씨정보 API 요청 시 토큰 제거

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -6,6 +6,7 @@ import { WeatherRequest, WeatherType } from '../types/api/weather';
 export const getWeather = async (params: WeatherRequest) => {
   const { data } = await instance.get<WeatherType>('/api/weather', {
     params,
+    headers: { 'No-Auth': 'True' },
   });
 
   return data;
