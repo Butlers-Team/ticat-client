441f8ae9a6cd1ea2d76fef1950660c3c32a24cef
leekoby <abc2752@naver.com>
2023-08-02 22:40:49 +0900

    feat: 토큰 시간 만료 refresh 요청 타입 추가

---
+++ src/types/auth/refresh.ts
@@ -0,0 +1,11 @@
+// ============================== 토큰 갱신 ==============================
+/** 2023/08/02 - 토큰 갱신 요청 타입 - by leekoby */
+export interface ApiRefreshRequest {}
+
+/** 2023/08/02 - 토큰 갱신 응답 타입 - by leekoby */
+export interface ApiRefreshResponse {}
+
+/** 2023/08/02 - 토큰 갱신 핸들러 - by leekoby */
+export interface ApiRefreshHandler {
+  (body: ApiRefreshRequest): Promise<ApiRefreshResponse>;
+}
