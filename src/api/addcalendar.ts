91dcd600a9b65c649bc3f5c5940bcb5a8f987cf5
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-09-02 02:47:12 +0900

    feat : 캘린더 추가 요청

--- src/api/addcalendar.ts
+++ src/api/addcalendar.ts
@@ -4,7 +4,15 @@ import instance from '@api/axiosInstance';
 import { CalendarAddRequest } from 'types/api/addcalendar';
 
 export const addCalendarRequest = async (params: CalendarAddRequest) => {
-  const { data } = await instance.post<string>(`/calendar/save`, { params });
+  const { data } = await instance.post<string>(
+    `/calendar/save`,
+    { params },
+    {
+      headers: {
+        'Content-Type': `application/json`,
+      },
+    },
+  );
 
   return data;
 };
