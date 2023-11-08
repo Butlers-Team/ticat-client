b993f1d22e4ec2a0237ffa45f203698d1693f2d9
parksubeom <sooknise@naver.com>
2023-11-08 14:40:49 +0900

    refactor :  디테일페이지 알럿창 토스트로 변경

--- src/api/calendar.ts
+++ src/api/calendar.ts
@@ -4,6 +4,7 @@ import instance from '@api/axiosInstance';
 import { CalendarListRequest, CalendarListListType } from 'types/api/calendar';
 import { CalendarAddRequest } from 'types/api/addcalendar';
 import { AxiosError } from 'axios';
+
 /** 2023/08/04 - 캘린더 축제 GET 요청 - by parksubeom */
 export const getCalendarList = async (params: CalendarListRequest) => {
   const { data } = await instance.get<CalendarListListType>('/members/schedule', {
@@ -16,13 +17,11 @@ export const getCalendarList = async (params: CalendarListRequest) => {
 export const addCalendarRequest = async ({ ...params }: CalendarAddRequest) => {
   try {
     const { data } = await instance.post<string>(`/calendar/save`, { ...params });
-    alert('성공적으로 추가되었습니다.');
     return data;
   } catch (error) {
     const axiosError = error as AxiosError;
     if (axiosError.response && axiosError.response.status === 409) {
       console.error('409 에러: 중복된 일정입니다.');
-      alert('중복된 일정입니다. 캘린더를 확인 부탁드립니다.');
     } else {
       throw new Error('일정 추가 중 오류가 발생했습니다.');
     }
